c331644ec0c85445ad01b15b745ec7da8ab92109
brahimjarallah <brahim.jarallah@gmail.com>
2021-06-17 20:39:16 +0100

    random-quote-machine-usestate upadated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import QuoteMachine from "./components/QuoteMachine"
 import "typeface-roboto"
 import { Grid, withStyles } from "@material-ui/core"
 
-
-
 const styles = {
   container: {
     alignItems: "center",
@@ -14,31 +12,25 @@ const styles = {
   },
 }
 
-function App({classes}) {
-
-  
+function App({ classes }) {
   const [quotes, setQuotes] = useState([])
   const [selectedQuoteIndex, setSelectedQuoteIndex] = useState(null)
 
-useEffect(async() => {
-   const data = await fetch(
+  useEffect(async () => {
+    const data = await fetch(
       "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
-   )
-  const quotes = await data.json()
-  setQuotes(quotes)
-  setSelectedQuoteIndex(random(0, quotes.length -1))
-},[])
+    )
+    const quotes = await data.json()
+    setQuotes(quotes)
+    setSelectedQuoteIndex(random(0, quotes.length - 1))
+  }, [])
 
-
-function getSelectedQuote() {
-  if (
-    !quotes.length || 
-    !Number.isInteger(selectedQuoteIndex)
-  ) {
-    return undefined
+  function getSelectedQuote() {
+    if (!quotes.length || !Number.isInteger(selectedQuoteIndex)) {
+      return undefined
+    }
+    return quotes[selectedQuoteIndex]
   }
-  return quotes[selectedQuoteIndex]
-}
 
   function generateNewQuoteIndex() {
     if (!quotes.length) {
@@ -48,42 +40,42 @@ function getSelectedQuote() {
   }
 
   function assignNewQuoteIndex() {
-   setSelectedQuoteIndex(generateNewQuoteIndex())
+    setSelectedQuoteIndex(generateNewQuoteIndex())
   }
-    // const {indexColor} = this.state
-    const colors = [
-      "#16a085",
-      "#27ae60",
-      "#2c3e50",
-      "#f39c12",
-      "#e74c3c",
-      "#9b59b6",
-      "#FB6964",
-      "#342224",
-      "#472E32",
-      "#BDBB99",
-      "#77B1A9",
-      "#73A857",
-    ]
-  const indexColor = random(0, colors.length -1)
-    return (
-      <Grid
-        style={{ backgroundColor: colors[indexColor] }}
-        className={classes.container}
-        id="quote-box"
-        justify="center"
-        container
-      >
-        <Grid xs="11" lg="8" item>
-          {getSelectedQuote() ? (
-            <QuoteMachine
-              selectedQuote={getSelectedQuote()}
-              assignNewQuoteIndex={assignNewQuoteIndex}
-            />
-          ) : null}
-        </Grid>
+  // const {indexColor} = this.state
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857",
+  ]
+  const indexColor = random(0, colors.length - 1)
+  return (
+    <Grid
+      style={{ backgroundColor: colors[indexColor] }}
+      className={classes.container}
+      id="quote-box"
+      justify="center"
+      container
+    >
+      <Grid xs="11" lg="8" item>
+        {getSelectedQuote() ? (
+          <QuoteMachine
+            selectedQuote={getSelectedQuote()}
+            assignNewQuoteIndex={assignNewQuoteIndex}
+          />
+        ) : null}
       </Grid>
-    )
+    </Grid>
+  )
 }
 
 export default withStyles(styles)(App)
